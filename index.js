e7b6e54818d2ef6cccf723108e18447c9e896686
karancode <karanthanvi0@gmail.com>
2018-08-31 13:17:35 +0900

    bug fix

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const HelpIntentHandler = {
 const CancelAndStopIntentHandler = {
     canHandle(handlerInput) {
         return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-        && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent')
+        && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
             || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
     },
     handle(handlerInput) {
