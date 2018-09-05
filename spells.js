7269d2c3d51b2581521db5a2b116720acbda2741
kdthanvi <thanvikaran95@gmail.com>
2018-09-05 11:06:16 +0900

    add spells

---
+++ spells.js
@@ -0,0 +1,66 @@
+module.exports = {
+
+    COLD: {
+        CHOCOLATE: [
+            'A venti coffee frappucino with two scoops of ice, five pumps of frap roast, and double blended.',//bad
+            'Non-Fat Frappuccino With Extra Whipped Cream And Chocolate Sauce',//l3
+        ],
+        VANILLA: [
+            'Cotton Candy Frappuccino : Say, Vanilla Bean Frappuccino with raspberry syrup. Add Whipped Cream.',
+            'Chocolate-Covered Raspberry Frappuccino : Say, Vanilla Bean Frappuccino with java chips, raspberry syrup (two pumps for a tall, three pumps for a grande and venti), mocha sauce, and mocha drizzle.',
+            'Java Berry Frappuccino : Say, Vanilla Bean Frappuccino with strawberry sauce and java chips. Add whipped cream and mocha drizzle.',
+            'A venti coffee with 10 Splenda packets and whipped cream',//bad
+            'A trenta vanilla sweet cream cold brew with two pumps of vanilla, three pumps of caramel syrup, two pumps of cinnamon dolce syrup, two pumps of hazelnut, two pumps of toffee nut syrup, two pumps of mocha, two pumps of white mocha, two pumps of pumpkin sauce, three pumps of maple pecan syrup, and five shots of espresso.',//bad
+            'A grande iced caramel macchiato with 10 pumps of sugar free vanilla and 17 Splenda packets.',//bad
+            'A venti caramel frappucino with one pump of caramel sauce, one pump of caramel syrup, one pump of mocha, one pump of white mocha, one pump of hazelnut, and an affogato shot.',//bad
+            'Grande, Iced, Sugar-Free, Vanilla Latte With Soy Milk',//l3
+
+        ],
+        MANGO: [
+            'Strawberry Shaken Lemonade : Say, Lemonade with strawberry flavoring, and shake with ice.,',
+            'Valentine Frappuccino : Say, Strawberries and Creme Frappuccino with raspberry syrup, vanilla bean in the middle, topped with blackberries',
+            'Captain Crunch Frappuccino : Say, Strawberries and Creme Frappuccino — plus caramel syrup, toffee syrup, hazelnut, and java chips.',
+            'Love Bean Frappuccino : Say, Vanilla Bean Frappuccino with raspberry-infused whipped cream and chocolate curls. ',
+            'A venti pumpkin spice latte with eight shots of espresso, seven pumps of pumpkin sauce, and one pump of maple pecan sauce.',//bad
+            'A grande ice water with seven pumps of raspberry and seven pumps of classic syrup.',//bad
+            'A venti caramel latte with black coffee instead of espresso, six pumps of caramel, five pumps of vanilla, four pumps of cinammon dolce, three pumps of butter pecan, and one pump of ameretto.',//bad
+            'A venti mango black tea lemonade with 24 pumps of mango',//bad
+
+        ],
+        MOCHA: [
+            'Passion Tea Frappuccino : Say, Milk, passion tea, three pumps of classic syrup, and creme base.',
+            'Raspberry Milk Tea: Say, Iced black tea made with half and half instead of water - add toffee nut syrup, raspberry syrup, and an extra pump of classic syrup.',
+            'Raspberry Cheesecake Mocha Frappuccino: Say, White Chocolate Mocha Frappuccino with raspberry syrup and whipped cream.',//bad
+            'A venti salted caramel mocha frappucino with five pumps of frap roast, four pumps of caramel sauce, four pumps of caramel syrup, three pumps of mocha, three pumps of toffee nut syrup, double blended with extra whipped cream.',//bad
+            'A trenta iced coffee with cream, 20 pumps of raspberry, and 20 pumps of white mocha.',//bad
+            'A doppio espresso with 20 shots of espresso and 10 pumps of white mocha.',//bad
+            'A venti green tea frappucino with a strawberry smoothie base, two pumps of caramel, three espresso shots, and topped with whipped cream and a caramel drizzle.',//bad
+        ]
+    },
+    HOT: {
+        CHOCOLATE: [
+            'Hot Chocolate Category : Empty',
+        ],
+        VANILLA: [
+            'Tall, Non-Fat Latte With Caramel Drizzle', //l3
+            'Triple, Venti, Half Sweet, Non-Fat, Caramel Macchiato',//l3
+            'Decaf, Soy Latte With An Extra Shot And Cream',//l3
+
+        ],
+        MANGO: [
+            'Pricky : Say, Tall, Half-Caff, Soy Latte At 120 Degrees',//l3
+        ],
+        MOCHA: [
+            'Triple, Venti, Soy, No Foam Latte',//l3
+            'Grande, Quad, Nonfat, One-Pump, No-Whip, Mocha',//l3
+
+        ]
+    }
+//special
+/*
+Double Ristretto Venti Half-Soy Nonfat Decaf Organic Chocolate Brownie Iced Vanilla Double-Shot Gingerbread Frappuccino Extra Hot With Foam Whipped Cream Upside Down Double Blended, One Sweet'N Low and One Nutrasweet, and Ice,
+Quad long shot grande in a venti cup half calf double cupped no sleeve salted caramel mocha latte with 2 pumps of vanilla substitute 2 pumps of white chocolate mocha for mocha and substitute 2 pumps of hazelnut for  toffee nut half whole milk and half breve with no whipped cream extra hot extra foam extra caramel drizzle extra salt add a scoop of vanilla bean powder with light ice well stirred,
+
+*/
+
+}
